92ee8e4a4863d2661a8ee66c704f62b1b16da438
Jesse <jesse.ballera@gmail.com>
2025-07-14 06:37:17 +0800

    rename default db

--- app-api/src/main/java/com/purplemango/app/repository/accommodations/AccommodationRepositoryImpl.java
+++ app-api/src/main/java/com/purplemango/app/repository/accommodations/AccommodationRepositoryImpl.java
@@ -4,6 +4,7 @@ import com.purplemango.app.aop.operations.BeforeGlobalMongoOperation;
 import com.purplemango.app.config.MultiTenantMongoDBFactory;
 import com.purplemango.app.model.accommodation.Accommodation;
 import com.purplemango.app.repository.MongoBaseRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -13,11 +14,12 @@ import java.util.List;
 @BeforeGlobalMongoOperation
 public class AccommodationRepositoryImpl extends MongoBaseRepository<Accommodation> implements AccommodationRepository {
     public static final String COLLECTION_NAME = "accommodations";
-    public static final String DATABASE_NAME = "tenants";
+    public static final String DATABASE_NAME = "i-appointment";
     private final MongoTemplate mongoTemplate;
 
-    public AccommodationRepositoryImpl(MongoTemplate mongoTemplate) {
-        super.setDatabaseName(DATABASE_NAME);
+    public AccommodationRepositoryImpl(MongoTemplate mongoTemplate,
+            @Value("${spring.data.mongodb.database}") String databaseName) {
+        super.setDatabaseName(databaseName);
         this.mongoTemplate = mongoTemplate;
     }
 
--- app-api/src/main/java/com/purplemango/app/repository/tenants/TenantRepositoryImpl.java
+++ app-api/src/main/java/com/purplemango/app/repository/tenants/TenantRepositoryImpl.java
@@ -5,6 +5,7 @@ import com.purplemango.app.aop.operations.BeforeGlobalMongoOperation;
 import com.purplemango.app.model.tenant.Tenant;
 import com.purplemango.app.repository.MongoBaseRepository;
 import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -23,11 +24,11 @@ import java.util.Optional;
 public class TenantRepositoryImpl extends MongoBaseRepository<Tenant> implements TenantRepository {
 
     public static final String COLLECTION_NAME = "tenants";
-    public static final String DATABASE_NAME = "tenants";
     private final MongoTemplate mongoTemplate;
 
-    public TenantRepositoryImpl(MongoTemplate mongoTemplate) {
-        super.setDatabaseName(DATABASE_NAME);
+    public TenantRepositoryImpl(MongoTemplate mongoTemplate,
+                                @Value("${spring.data.mongodb.database}") String databaseName) {
+        super.setDatabaseName(databaseName);
         this.mongoTemplate = mongoTemplate;
     }
     @Override
